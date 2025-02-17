2acb87bcb24d7f9a7f3faaf5efcaf683ac98ead2
vladimir <vovka1998@gmail.com>
2025-02-17 17:15:37 +0300

    added view for API-endpoint

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
+from django.http import HttpRequest, JsonResponse
+from django.views.generic import View
+from django.db import transaction
+from pydantic import ValidationError
 
-# Create your views here.
+from .models import Robot
+from .schemas import RobotBase
+
+
+class AddRobot(View):
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        try:
+            data: dict = json.loads(request.body)
+            robots = []
+            for data in data.values():
+                serial_robot = f"{data['model']}-{data['version']}"
+                data_robot = RobotBase(serial=serial_robot, model=data["model"], version=data["version"])
+                robots.append(
+                    Robot(**data_robot.model_dump())
+                )
+            with transaction.atomic():
+                Robot.objects.bulk_create(robots)
+            return JsonResponse({"success": True}, status=200)
+        except ValidationError as e:
+            return JsonResponse({"error": str(e)}, status=403)
