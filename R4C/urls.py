d3dd69ad3ebcccade3efb2b9b97d6a223c8bc570
vladimir <vovka1998@gmail.com>
2025-02-18 14:14:37 +0300

    connected django-debug-toolbar

--- R4C/urls.py
+++ R4C/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+
+from R4C import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("robots/", include("robots.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
