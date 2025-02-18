555ae94708b2987cecf5d15c15f4382a067c283a
vladimir <vovka1998@gmail.com>
2025-02-18 15:26:44 +0300

    connected view to project

--- robots/urls.py
+++ robots/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import AddRobot
+from .views import ExportToExcel
 
 app_name = "robots"
 
 urlpatterns = [
     path("add/", AddRobot.as_view(), name="add"),
+    path("export/", ExportToExcel.as_view(), name="export")
 ]
