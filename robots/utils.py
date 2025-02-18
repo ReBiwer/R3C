6ea4808172070dc35ab730a2f40d230a52800c38
vladimir <vovka1998@gmail.com>
2025-02-18 15:25:54 +0300

    added utils

---
+++ robots/utils.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from django.utils import timezone
+from django.db.models import Count
+from .models import Robot
+from .schemas import RobotToExcel
+from pandas import DataFrame
+
+
+def get_info_robots() -> list[RobotToExcel]:
+    """Возвращает данные из БД валидированные pydantic схемой"""
+    one_week_ago = timezone.now() - timedelta(days=7)
+    robots = (
+        Robot.objects.
+        filter(created__gte=one_week_ago).
+        values("model", "version").
+        annotate(total=Count('id'))
+    )
+    result = [RobotToExcel.model_validate(robot_data) for robot_data in robots]
+    return result
+
+def get_dataframe(data: list[RobotToExcel]) -> DataFrame:
+    """Преобразовывает информацию о роботах в DataFrame"""
+    name_columns = {'model': 'Модель', 'version': 'Версия', 'total': 'Количество за неделю'}
+    data_robot = [robot.model_dump() for robot in data]
+    df = DataFrame(data_robot)
+    df.rename(columns=name_columns, inplace=True)
+    return df
