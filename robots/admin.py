0de664c6ff3f44ac81a042963a5b5f6937666654
vladimir <vovka1998@gmail.com>
2025-02-18 14:15:21 +0300

    added model Robot in admin

--- robots/admin.py
+++ robots/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Robot
 
-# Register your models here.
+@admin.register(Robot)
+class RobotAdmin(admin.ModelAdmin):
+    list_display = [
+        "serial",
+        "model",
+        "version",
+        "created",
+    ]
