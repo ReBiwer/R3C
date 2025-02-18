ecb2dfb96cf16c5d5696ef142be4b0f2366f7f5e
vladimir <vovka1998@gmail.com>
2025-02-18 15:26:10 +0300

    added schema for data from DB

--- robots/schemas.py
+++ robots/schemas.py
@@ -7,3 +7,10 @@ class RobotInfo(BaseModel):
     model: str
     version: str
     model_config = ConfigDict(from_attributes=True)
+
+
+class RobotToExcel(BaseModel):
+    model: str
+    version: str
+    total: int
+    model_config = ConfigDict(from_attributes=True)
