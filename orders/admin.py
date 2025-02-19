3e9b676f961ee69c4ecafb06b75130fd4d709453
vladimir <vovka1998@gmail.com>
2025-02-19 15:03:18 +0300

    added models in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "customer",
+        "robot_serial",
+    ]
